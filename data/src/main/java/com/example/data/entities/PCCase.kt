e8d30378424f3cc4ce87d1a4dd6dc1f89341db35
Dinar Subeev <dinars163@gmail.com>
2024-05-31 20:05:23 +0400

    PcCase -> PCCase, изменено имя

--- data/src/main/java/com/example/data/entities/PCCase.kt
+++ data/src/main/java/com/example/data/entities/PCCase.kt
@@ -1,14 +1,14 @@
 package com.example.data.entities
 
 /**
- * Класс [PcCase]. Представляет собой модельку корпуса компьютера
+ * Класс [PCCase]. Представляет собой модельку корпуса компьютера
  * с параметрами:
  * @param id уникальный id
  * @param name имя модели
  * @param price цена
  * @param size размер
  */
-data class PcCase(
+data class PCCase(
     val id: Int,
     val name: String,
     val price: String,
