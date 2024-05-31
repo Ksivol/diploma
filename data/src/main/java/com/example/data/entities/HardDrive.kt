dcdc9294b1f813dda21f8405e3ae630b12423587
Dinar Subeev <dinars163@gmail.com>
2024-05-31 18:11:51 +0400

    +HardDrive дата-класс

---
+++ data/src/main/java/com/example/data/entities/HardDrive.kt
@@ -0,0 +1,10 @@
+package com.example.data.entities
+
+data class HardDrive(
+    val id: Int,
+    val name: String,
+    val price: String,
+    val type: Int,
+    val size: String,
+    val overwrite: Int
+)
