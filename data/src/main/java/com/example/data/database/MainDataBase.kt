715b92d83e71ea59c781396f2d26b30edde600ca
Dinar Subeev <dinars163@gmail.com>
2024-05-31 20:28:06 +0400

    Функции доступа к DAO

--- data/src/main/java/com/example/data/database/MainDataBase.kt
+++ data/src/main/java/com/example/data/database/MainDataBase.kt
@@ -4,10 +4,26 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.data.dao.CoolerDAO
+import com.example.data.dao.CpuDAO
+import com.example.data.dao.HardDriveDAO
+import com.example.data.dao.MotherboardDAO
+import com.example.data.dao.PcCaseDAO
+import com.example.data.dao.PsuDAO
+import com.example.data.dao.RamDAO
+import com.example.data.dao.VideoCardDAO
 
 @Database(entities = [], version = 1)
 abstract class MainDataBase: RoomDatabase() {
 
+    abstract fun getCoolerDAO(): CoolerDAO
+    abstract fun getCpuDAO(): CpuDAO
+    abstract fun getHardDriveDAO(): HardDriveDAO
+    abstract fun getMotherboardDAO(): MotherboardDAO
+    abstract fun getPcCaseDAO(): PcCaseDAO
+    abstract fun getPsuDAO(): PsuDAO
+    abstract fun getRamDAO(): RamDAO
+    abstract fun getVideoCardDAO(): VideoCardDAO
     companion object {
         private var INSTANCE: MainDataBase? = null
 
