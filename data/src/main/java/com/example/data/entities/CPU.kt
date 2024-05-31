3df201825df9a19db8307c10ebe6f9f89c807d9a
Dinar Subeev <dinars163@gmail.com>
2024-05-31 17:59:31 +0400

    +CPU дата-класс

---
+++ data/src/main/java/com/example/data/entities/CPU.kt
@@ -0,0 +1,10 @@
+package com.example.data.entities
+
+data class CPU(
+    val id: Int,
+    val name: String,
+    val socket: String,
+    val price: String,
+    val clockRate: String,
+    val wattage: String
+)
