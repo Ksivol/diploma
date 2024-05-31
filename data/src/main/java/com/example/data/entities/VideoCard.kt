c69921311d5849eb2cb3fc4522de7e8d05f492a5
Dinar Subeev <dinars163@gmail.com>
2024-05-31 18:02:31 +0400

    +VideoCard дата-класс

---
+++ data/src/main/java/com/example/data/entities/VideoCard.kt
@@ -0,0 +1,11 @@
+package com.example.data.entities
+
+data class VideoCard(
+    val id: Int,
+    val name: String,
+    val price: String,
+    val size: String,
+    val clockRate: String,
+    val wattage: String,
+    val videoMemory: String
+)
