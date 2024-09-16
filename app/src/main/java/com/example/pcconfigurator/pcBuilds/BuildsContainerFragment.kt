78783f4cebb52705ad064d16edeb789c57316820
Dinar Subeev <dinars163@gmail.com>
2024-09-16 14:32:57 +0400

    Контейнер для builds

---
+++ app/src/main/java/com/example/pcconfigurator/pcBuilds/BuildsContainerFragment.kt
@@ -0,0 +1,6 @@
+package com.example.pcconfigurator.pcBuilds
+
+import androidx.fragment.app.Fragment
+import com.example.pcconfigurator.R
+
+class BuildsContainerFragment : Fragment(R.layout.fragment_builds_container)
