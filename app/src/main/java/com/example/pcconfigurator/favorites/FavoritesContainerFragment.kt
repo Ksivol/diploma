cb8f066833926083f09e653b170492387bc4581c
Ksivol <surkin-ant@mail.com>
2024-10-20 15:25:25 +0400

    Создана Favorites фрагмент и ContainerFavorites фрагмент

---
+++ app/src/main/java/com/example/pcconfigurator/favorites/FavoritesContainerFragment.kt
@@ -0,0 +1,13 @@
+package com.example.pcconfigurator.favorites
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pcconfigurator.R
+
+
+class FavoritesContainerFragment : Fragment(R.layout.fragment_favorites_container) {
+
+}
