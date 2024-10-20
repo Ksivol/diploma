7ac2959e9f6c73db2892fe80f908017e1b6b5f88
Ksivol <surkin-ant@mail.com>
2024-10-20 14:57:19 +0400

    Создан фрагмент и класс категорий и фрагмент и класс их контейнера

---
+++ app/src/main/java/com/example/pcconfigurator/categories/CategoriesContainerFragment.kt
@@ -0,0 +1,9 @@
+package com.example.pcconfigurator.categories
+
+import androidx.fragment.app.Fragment
+import com.example.pcconfigurator.R
+
+
+class CategoriesContainerFragment : Fragment(R.layout.fragment_categories_container) {
+
+}
