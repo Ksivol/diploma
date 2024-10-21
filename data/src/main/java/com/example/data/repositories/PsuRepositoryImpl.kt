2d6c7ea970708d6b45fd7017cb163fbae992fdf6
Dinar Subeev <dinars163@gmail.com>
2024-10-21 04:08:29 +0400

    Инъекции

--- data/src/main/java/com/example/data/repositories/PsuRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repositories/PsuRepositoryImpl.kt
@@ -8,23 +8,26 @@ import com.example.domain.enitities.PSU
 import com.example.domain.repositories.PsuRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
-class PsuRepositoryImpl(private val psuDAO: PsuDAO) : PsuRepository {
-    override fun getPsus(): Flow<List<PSU>> = psuDAO.getPsus().map { it.map(PSUEntity::toDomain) }
+class PsuRepositoryImpl
+    @Inject
+    constructor(
+        private val psuDAO: PsuDAO,
+    ) : PsuRepository {
+        override fun getPsus(): Flow<List<PSU>> = psuDAO.getPsus().map { it.map(PSUEntity::toDomain) }
 
-    override fun getPsu(id: Int): PSU {
-        return psuDAO.getPsu(id).toDomain()
+        override fun getPsu(id: Int): PSU = psuDAO.getPsu(id).toDomain()
+
+        override fun insertPsu(psu: PSU) {
+            psuDAO.insertPsu(psu.toData())
+        }
+
+        override fun updatePsu(psu: PSU) {
+            psuDAO.updatePsu(psu.toData())
+        }
+
+        override fun deletePsu(psu: PSU) {
+            psuDAO.deletePsu(psu.toData())
+        }
     }
-
-    override fun insertPsu(psu: PSU) {
-        psuDAO.insertPsu(psu.toData())
-    }
-
-    override fun updatePsu(psu: PSU) {
-        psuDAO.updatePsu(psu.toData())
-    }
-
-    override fun deletePsu(psu: PSU) {
-        psuDAO.deletePsu(psu.toData())
-    }
-}
