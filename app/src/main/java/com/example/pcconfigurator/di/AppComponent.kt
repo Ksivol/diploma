0f5871d9f03c0efe422aa8800878da304d8e2e71
Dinar Subeev <dinars163@gmail.com>
2024-11-03 14:32:31 +0400

    +inject функция

--- app/src/main/java/com/example/pcconfigurator/di/AppComponent.kt
+++ app/src/main/java/com/example/pcconfigurator/di/AppComponent.kt
@@ -7,6 +7,7 @@ import com.example.pcconfigurator.features.categories.CategoriesFragment
 import com.example.pcconfigurator.features.favorites.FavoritesFragment
 import com.example.pcconfigurator.features.pcBuilds.BuildsFragment
 import com.example.pcconfigurator.features.profile.ProfileFragment
+import com.example.pcconfigurator.main.MainContainerFragment
 import dagger.BindsInstance
 import dagger.Component
 
@@ -19,6 +20,8 @@ interface AppComponent {
     fun inject(favoritesFragment: FavoritesFragment)
 
     fun inject(profileFragment: ProfileFragment)
+
+    fun inject(mainContainerFragment: MainContainerFragment)
 
     @Component.Builder
     interface Builder {
