cca965e0453becdbeb4a20ae69f16e830bb6362b
Dinar Subeev <dinars163@gmail.com>
2024-11-03 12:31:34 +0400

    Вьюмоделька для далнейших решений

---
+++ app/src/main/java/com/example/pcconfigurator/main/MainViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.pcconfigurator.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    private val innerTitle: MutableStateFlow<String> = MutableStateFlow("")
+    val title = innerTitle.asStateFlow()
+
+    fun setTitle(string: String) =
+        viewModelScope.launch(Dispatchers.IO) {
+            innerTitle.emit(string)
+        }
+}
