decdc4a14be15a6959ac68a01107c2f5fd484e83
Dinar Subeev <dinars163@gmail.com>
2024-12-08 20:27:43 +0400

    Настройки для грамотного отображения цвета иконок статус бара при светлой теме

--- app/src/main/java/com/example/pcconfigurator/main/MainActivity.kt
+++ app/src/main/java/com/example/pcconfigurator/main/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.pcconfigurator.main
 
 import android.content.Context
 import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pcconfigurator.R
@@ -10,7 +12,7 @@ import com.example.pcconfigurator.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(SystemBarStyle.auto(Color.WHITE, Color.WHITE) { true })
         setContentView(R.layout.activity_main)
         window.statusBarColor = if (isDarkTheme(this)) getColor(R.color.colorPrimaryDark) else getColor(R.color.colorPrimaryLight)
     }
