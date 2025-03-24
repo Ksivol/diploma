80a925c2b94cecea8549e441c725a0ead7ec1396
Dinar Subeev <dinar.subeev@simbirsoft.com>
2025-03-24 19:07:59 +0400

    Новая вм

---
+++ app/src/main/java/com/example/pcconfigurator/main/MainContainerViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.pcconfigurator.main
+
+import androidx.lifecycle.ViewModel
+import com.example.pcconfigurator.features.pcbuildsfeature.builds.BuildsFragment
+import com.github.terrakok.cicerone.Router
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+import javax.inject.Inject
+
+class MainContainerViewModel @Inject constructor(private val router: Router) : ViewModel() {
+
+    fun navigateTo(screen: FragmentScreen) {
+        router.replaceScreen(screen)
+    }
+
+    fun back() {
+        router.exit()
+    }
+}
