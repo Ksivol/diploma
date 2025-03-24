32b8aec29ad5749c0c4405f6b3c303d835e093f6
Dinar Subeev <dinar.subeev@simbirsoft.com>
2025-03-24 19:08:05 +0400

    + новый модуль

---
+++ app/src/main/java/com/example/pcconfigurator/di/MainModule.kt
@@ -0,0 +1,33 @@
+package com.example.pcconfigurator.di
+
+import com.example.pcconfigurator.R
+import com.example.pcconfigurator.main.App
+import com.example.pcconfigurator.main.MainActivity
+import com.github.terrakok.cicerone.Cicerone
+import com.github.terrakok.cicerone.Navigator
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.Router
+import com.github.terrakok.cicerone.androidx.AppNavigator
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class MainModule {
+
+    @Provides
+    @Singleton
+    fun provideCicerone(): Cicerone<Router> = Cicerone.create()
+
+    @Provides
+    @Singleton
+    fun provideRouter(cicerone: Cicerone<Router>): Router = cicerone.router
+
+    @Provides
+    @Singleton
+    fun provideNavigatorHolder(cicerone: Cicerone<Router>): NavigatorHolder = cicerone.getNavigatorHolder()
+
+    @Provides
+    @Singleton
+    fun provideMainNavigator(activity: MainActivity): Navigator = AppNavigator(activity, R.id.main)
+}
