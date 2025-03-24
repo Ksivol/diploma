fde0a5e19d381c0f8646263535b682dd769d406f
Dinar Subeev <dinar.subeev@simbirsoft.com>
2025-03-24 19:07:46 +0400

    Инъекция навигации

--- app/src/main/java/com/example/pcconfigurator/main/MainActivity.kt
+++ app/src/main/java/com/example/pcconfigurator/main/MainActivity.kt
@@ -4,15 +4,46 @@ import android.content.Context
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.pcconfigurator.R
+import com.example.pcconfigurator.di.component
+import com.github.terrakok.cicerone.Navigator
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.androidx.AppNavigator
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var navigatorHolder: NavigatorHolder
+
+    @Inject
+    lateinit var factory: ViewModelProvider.Factory
+
+    private val mainViewModel: MainViewModel by viewModels { factory }
+
+    private val navigator: Navigator = AppNavigator(this, R.id.main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        component.inject(this)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        window.statusBarColor = if (isDarkTheme(this)) getColor(R.color.colorPrimaryDark) else getColor(R.color.colorPrimaryLight)
+        window.statusBarColor =
+            if (isDarkTheme(this)) getColor(R.color.colorPrimaryDark) else getColor(R.color.colorPrimaryLight)
+
+        mainViewModel.navigateToMainContainer()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        navigatorHolder.setNavigator(navigator)
+    }
+
+    override fun onPause() {
+        navigatorHolder.removeNavigator()
+        super.onPause()
     }
 
     private fun isDarkTheme(context: Context) =
