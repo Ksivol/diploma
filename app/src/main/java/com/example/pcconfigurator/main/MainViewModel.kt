de12070e7e82c2a4d377fd5f64e888ce0db9d65e
Dinar Subeev <dinar.subeev@simbirsoft.com>
2025-03-25 18:38:52 +0400

    Минорные изменения

--- app/src/main/java/com/example/pcconfigurator/main/MainViewModel.kt
+++ app/src/main/java/com/example/pcconfigurator/main/MainViewModel.kt
@@ -2,15 +2,13 @@ package com.example.pcconfigurator.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.github.terrakok.cicerone.Router
-import com.github.terrakok.cicerone.androidx.FragmentScreen
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(private val router: Router) : ViewModel() {
+class MainViewModel @Inject constructor() : ViewModel() {
     private val _title: MutableStateFlow<String> = MutableStateFlow("")
     val title = _title.asStateFlow()
 
