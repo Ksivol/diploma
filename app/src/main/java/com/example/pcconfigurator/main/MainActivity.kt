2713d6492f1a217d34dc94a4a2d215e267c2ad79
Dinar Subeev <dinar.subeev@simbirsoft.com>
2025-03-25 18:35:06 +0400

    Удалены ненужные зависимости, навигация осуществляется посредством системных средств

--- app/src/main/java/com/example/pcconfigurator/main/MainActivity.kt
+++ app/src/main/java/com/example/pcconfigurator/main/MainActivity.kt
@@ -4,14 +4,13 @@ import android.content.Context
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import androidx.lifecycle.ViewModelProvider
 import com.example.pcconfigurator.R
 import com.example.pcconfigurator.di.component
-import com.github.terrakok.cicerone.Navigator
 import com.github.terrakok.cicerone.NavigatorHolder
-import com.github.terrakok.cicerone.androidx.AppNavigator
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -21,10 +20,6 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var factory: ViewModelProvider.Factory
 
-    private val mainViewModel: MainViewModel by viewModels { factory }
-
-    private val navigator: Navigator = AppNavigator(this, R.id.main)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         component.inject(this)
         super.onCreate(savedInstanceState)
@@ -33,17 +28,9 @@ class MainActivity : AppCompatActivity() {
         window.statusBarColor =
             if (isDarkTheme(this)) getColor(R.color.colorPrimaryDark) else getColor(R.color.colorPrimaryLight)
 
-        mainViewModel.navigateToMainContainer()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        navigatorHolder.setNavigator(navigator)
-    }
-
-    override fun onPause() {
-        navigatorHolder.removeNavigator()
-        super.onPause()
+        supportFragmentManager.commit {
+            replace<MainContainerFragment>(R.id.main)
+        }
     }
 
     private fun isDarkTheme(context: Context) =
